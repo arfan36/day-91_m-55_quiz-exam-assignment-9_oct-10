c60583b2c7544ce6c83479534c3ca6ca7c1bd6c2
arfan36 <arfanur36@gmail.com>
2022-10-11 21:41:30 +0600

    header added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Header></Header>
         </div>
     );
 };
